e72f9bf1f5eaa423ee34ff8247b470867ed9e1f8
Julio Paez <julio.paez.oliveira@gmail.com>
2021-06-03 23:46:08 -0300

    Revert "Delete JavaProjectTest directory"
    This reverts commit 654d668804309b972d1ddcf7e99331c65aca5e3b.

---
+++ JavaProjectTest/src/ExecutorsFramework/AsynchronousAPI.java
@@ -0,0 +1,9 @@
+package ExecutorsFramework;
+
+public class AsynchronousAPI {
+    public static void main(String[] args){
+        var service = new EmailService();
+        service.sendAsync();
+        System.out.println("Hello world");
+    }
+}
